a5396a654949397193dc73780fcf359f29437c22
daria marczak <marczak.ria@gmail.com>
2017-10-27 20:22:32 +0200

    changed prompt to alert

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var a = 2;
 var h = 4271;
 
-triangleArea = (a*h)/2;
+var triangleArea = a * h / 2;
 
 console.log("Triangle field with base a: " + a + " and height h: " + h + "is equal to: " + triangleArea);
 
-prompt(triangleArea);
+alert(triangleArea);
